a612541f8812c6e6f1475be9bcd10ca22f0690dd
Brett Ian Balogh <giantmolecules@gmail.com>
2024-09-16 16:18:21 -0500

    updates

--- fading_leds/fading_leds.ino
+++ fading_leds/fading_leds.ino
@@ -4,9 +4,9 @@
 // Brett Ian Balogh
 // https://github.com/giantmolecules/ACTIVATED_OBJECTS_FA24
 //
-// marching_leds.ino
+// fading_leds.ino
 //
-// This example shows how to sequence 5 LEDs
+// This example shows how to fade 5 LEDs in sequence.
 //
 //----------------------------------------------------------------//
 
@@ -22,7 +22,7 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 const int pins[] = { 11, 10, 9, 6, 5 };
 
 // How long to wait on / off
-const int delayTime = 100;
+const int delayTime = 1;
 
 //----{SETUP}-----------------------------------------------------//
 
@@ -62,18 +62,48 @@ void setup() {
 //----{LOOP}------------------------------------------------------//
 
 void loop() {
-  for(int i = 0; i < 5; i++){
+  for (int i = 0; i < 5; i++) {
 
-    // Print to the tft
-    tft.setCursor(0,0);
-    tft.print("PIN: ");
-    tft.print(pins[i]);
-    tft.print("  ");
+    for (int pwm = 0; pwm <= 255; pwm++) {
+      
+      /*
+      // Print to the tft
+      tft.setCursor(0, 0);
+      tft.print("PIN:");
+      tft.print(pins[i]);
+      tft.println("  ");
+      tft.print("VAL:");
+      tft.print(pwm);
+      tft.println("   ");
+      */
 
-    digitalWrite(pins[i], HIGH);
-    delay(delayTime);
-    digitalWrite(pins[i], LOW);
-    delay(delayTime);
+      analogWrite(pins[i], pwm);
+      delay(delayTime);
+      analogWrite(pins[i], pwm);
+      delay(delayTime);
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+
+    for (int pwm = 255; pwm >= 0; pwm--) {
+      
+      /*
+      // Print to the tft
+      tft.setCursor(0, 0);
+      tft.print("PIN:");
+      tft.print(pins[i]);
+      tft.println("  ");
+      tft.print("VAL:");
+      tft.print(pwm);
+      tft.println("   ");
+      */
+
+      analogWrite(pins[i], pwm);
+      delay(delayTime);
+      analogWrite(pins[i], pwm);
+      delay(delayTime);
+    }
   }
 }
 
