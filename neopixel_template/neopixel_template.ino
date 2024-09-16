cb7b1998f862328104a9ea1808eb48af3ca1bcc7
Brett Ian Balogh <giantmolecules@gmail.com>
2024-09-16 15:08:01 -0500

    garfield image reader demo

--- neopixel_template/neopixel_template.ino
+++ neopixel_template/neopixel_template.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 33
+#define LED_PIN 5
 
-#define LED_COUNT 1
+#define LED_COUNT 12
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
 void setup() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -14,16 +14,36 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 255; i++) {
-    strip.setPixelColor(0, strip.Color(0, i, 255 - i));
-    strip.setPixelColor(1, strip.Color(0, 255 - i, i));
-    strip.show();
-    delay(10);
+
+  //This is a test. This is only a test.
+  rainbow(10);
+  
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
   }
-  for (int i = 255; i > 0; i--) {
-    strip.setPixelColor(0, strip.Color(0, i, 255 - i));
-    strip.setPixelColor(1, strip.Color(0, 255 - i, i));
-    strip.show();
-    delay(10);
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
   }
 }
