0ae9cd301d24eb056bd7389867992bb47967c438
Brett Ian Balogh <giantmolecules@gmail.com>
2024-09-30 18:07:28 -0500

    Create servo_sweep.ino

---
+++ servo_sweep/servo_sweep.ino
@@ -0,0 +1,98 @@
+//----------------------------------------------------------------//
+//
+// SAIC Activated Objects Fall 2024
+// Brett Ian Balogh
+// https://github.com/giantmolecules/ACTIVATED_OBJECTS_FA24
+//
+// servo_sweep.ino
+//
+// Moves a servo back and forth in it's range of motion.
+// Uses ESP32Servo.h, not the default arduino servo library.
+//
+//----------------------------------------------------------------//
+
+// Include libraries for TFT
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+// Include the ESP32 Arduino Servo Library instead of the original Arduino Servo Library
+#include <ESP32Servo.h>
+
+// Create a TFT object
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// Create servo object to control a servo
+Servo myservo;
+
+// Possible PWM GPIO pins on the ESP32: 0(used by on-board button),2,4,5(used by on-board LED),12-19,21-23,25-27,32-33
+int servoPin = 12;  // GPIO pin used to connect the servo control (digital out)
+// Variable to hold servo position
+int pos = 0;
+
+
+//----{SETUP}-----------------------------------------------------//
+
+void setup() {
+
+  // Start Serial COM for debugging
+  Serial.begin(115200);
+
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240);  // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  // Turn off word wrapping
+  tft.setTextWrap(false);
+
+  // default text size
+  tft.setTextSize(4);
+
+  // set text foreground and background colors
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);           // Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400);  // attaches the servo on pin 18 to the servo object
+  // using SG90 servo min/max of 500us and 2400us
+  // for MG995 large servo, use 1000us and 2000us,
+  // which are the defaults, so this line could be
+  // "myservo.attach(servoPin);"
+}
+
+//----{LOOP}------------------------------------------------------//
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);  // tell servo to go to position in variable 'pos'
+    tft.setCursor(0, 0);
+    tft.println("Angle:");
+    tft.print(pos);
+    tft.print("   ");
+    delay(15);  // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
+    myservo.write(pos);                  // tell servo to go to position in variable 'pos'
+    tft.setCursor(0, 0);
+    tft.println("Angle:");
+    tft.print(pos);
+    tft.print("   ");
+    delay(15);  // waits 15ms for the servo to reach the position
+  }
+}
+
+//----{END}------------------------------------------------------//
