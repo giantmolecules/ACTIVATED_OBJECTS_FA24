41d598067cad782aaf53709a94fe74d934e34948
Brett Ian Balogh <giantmolecules@gmail.com>
2024-10-01 10:54:36 -0500

    Update simplest_pir.ino

--- simplest_pir/simplest_pir.ino
+++ simplest_pir/simplest_pir.ino
@@ -1,11 +1,22 @@
+//----------------------------------------------------------------//
+//
+// SAIC Activated Objects Fall 2024
+// Brett Ian Balogh
+// https://github.com/giantmolecules/ACTIVATED_OBJECTS_FA24
+//
+// simplest_pir.ino
+//
+// Demonstrates the use of a passive infrared sensor (PIR). Uses
+// interrupts. Uses board version 2.0.14 and GFX version 1.11.0
+//
+//----------------------------------------------------------------//
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(9, INPUT);
   pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(digitalRead(9)){
     digitalWrite(5, HIGH);
   }
