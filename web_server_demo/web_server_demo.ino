73dc678f7d416dc184b7a0398f28963ae19288be
Brett Ian Balogh <giantmolecules@gmail.com>
2024-10-15 13:06:18 -0500

    update

---
+++ web_server_demo/web_server_demo.ino
@@ -0,0 +1,133 @@
+//----------------------------------------------------------------//
+//
+// SAIC Activated Objects Fall 2024
+// Brett Ian Balogh
+// https://github.com/giantmolecules/ACTIVATED_OBJECTS_FA24
+//
+// web_control_servo.ino
+//
+// Shoes how to serve a webpage from the ESP32 with a form that 
+// controls the position of a servo motor.
+//
+//----------------------------------------------------------------//
+
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/stepper-motor-esp32-web-server/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+#include <ESP32Servo.h>
+
+// Create servo object to control a servo
+Servo myservo;
+
+// Possible PWM GPIO pins on the ESP32: 0(used by on-board button),2,4,5(used by on-board LED),12-19,21-23,25-27,32-33
+int servoPin = 12;  // GPIO pin used to connect the servo control (digital out)
+// Variable to hold servo position
+int pos = 0;
+
+// Replace with your network credentials
+const char* ssid = "SONGBIRD";
+const char* password = "quietcartoon195";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameter in HTTP POST request
+const char* PARAM_INPUT_1 = "direction";
+const char* PARAM_INPUT_2 = "steps";
+
+//Variables to save values from HTML form
+String direction;
+String steps;
+
+bool newRequest = false;
+
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  } else {
+    Serial.println("LittleFS mounted successfully");
+  }
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  initWiFi();
+  initLittleFS();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest* request) {
+    int params = request->params();
+    for (int i = 0; i < params; i++) {
+      const AsyncWebParameter* p = request->getParam(i);
+      if (p->isPost()) {
+        // HTTP POST input1 value
+        if (p->name() == PARAM_INPUT_1) {
+          direction = p->value().c_str();
+          Serial.print("Direction set to: ");
+          Serial.println(direction);
+        }
+        // HTTP POST input2 value
+        if (p->name() == PARAM_INPUT_2) {
+          steps = p->value().c_str();
+          Serial.print("Number of steps set to: ");
+          Serial.println(steps);
+          // Write file to save value
+        }
+        newRequest = true;
+        //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      }
+    }
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.begin();
+
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);           // Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400);  // attaches the servo on pin 18 to the servo object
+}
+
+void loop() {
+  if (newRequest) {
+    pos = steps.toInt();
+    pos = constrain(pos, 0, 180);
+    myservo.write(pos);
+    newRequest = false;
+  }
+}
