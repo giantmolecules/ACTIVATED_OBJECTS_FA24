ec971e91dd1e693d62f98cba9d762d36563c22f6
Brett Ian Balogh <giantmolecules@gmail.com>
2024-10-22 14:17:43 -0500

    update

---
+++ analog_serial_sender/analog_serial_sender.ino
@@ -0,0 +1,68 @@
+//----------------------------------------------------------------//
+//
+// SAIC Activated Objects Fall 2024
+// Brett Ian Balogh
+// https://github.com/giantmolecules/ACTIVATED_OBJECTS_FA24
+//
+// analog_serial_sender.ino
+//
+// Reads a potentiomenter and sends analog value to Serial.
+// Used as a test to send data to Max/MSP, Pd
+//
+//----------------------------------------------------------------//
+
+// Include libraries for TFT
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+
+// Create a TFT object
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+//----{SETUP}-----------------------------------------------------//
+
+void setup() {
+
+  // Start Serial COM
+  Serial.begin(115200);
+  
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  // Turn off word wrapping
+  tft.setTextWrap(false);
+
+  // default text size
+  tft.setTextSize(4);
+  
+  // set text foreground and background colors
+  tft.setTextColor(ST77XX_WHITE,ST77XX_BLACK);
+
+  Serial.println(F("TFT Initialized"));
+
+}
+
+//----{LOOP}------------------------------------------------------//
+
+void loop() {
+ int val = analogRead(5);
+ Serial.println(val);
+ tft.setCursor(0,50);
+ tft.print("Val: ");
+ tft.print(val);
+ tft.print("   ");
+ delay(10);
+}
+
+//----{END}------------------------------------------------------//
