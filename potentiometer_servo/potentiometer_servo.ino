dc6f2c1842d78562f54a40456bd3882ee170b520
Brett Ian Balogh <giantmolecules@gmail.com>
2024-11-12 11:17:14 -0600

    update

--- potentiometer_servo/potentiometer_servo.ino
+++ potentiometer_servo/potentiometer_servo.ino
@@ -78,23 +78,16 @@ void setup() {
 //----{LOOP}------------------------------------------------------//
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    tft.setCursor(0, 0);
-    tft.println("Angle:");
-    tft.print(pos);
-    tft.print("   ");
-    delay(15);  // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
-    myservo.write(pos);                  // tell servo to go to position in variable 'pos'
-    tft.setCursor(0, 0);
-    tft.println("Angle:");
-    tft.print(pos);
-    tft.print("   ");
-    delay(15);  // waits 15ms for the servo to reach the position
-  }
+
+  int analogValue = analogRead(5);
+  pos = map(analogValue, 0, 8192, 0, 180);
+  pos = constrain(pos, 0, 180);
+  myservo.write(pos);  // tell servo to go to position in variable 'pos'
+  tft.setCursor(0, 0);
+  tft.println("Angle:");
+  tft.print(pos);
+  tft.print("   ");
+  delay(15);  // waits 15ms for the servo to reach the position
 }
 
 //----{END}------------------------------------------------------//
